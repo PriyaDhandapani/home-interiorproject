b41bcc2764fcacb15941566f96b550aa4afd3622
priyapandiandeveloper <priyapandiandeveloper@gmail.com>
2023-04-09 13:58:13 +0100

    "mediaquery is done"

--- script.js
+++ script.js
@@ -1,47 +1,14 @@
-// What is the result of 5 multiplied by 7?
+const subscribeBtn = document.getElementById("btn");
+subscribeBtn.addEventListener("click", function () {
+  showThankYouMessage();
+});
 
-// let a = 5;
-// let b = 7;
-// console.log("What is the result of 5 multiplied by 7 is " + a * b);
+function showThankYouMessage() {
+  const message = document.createElement("p");
+  message.textContent = "Thank you for subscribing!";
+  message.ClassList.add("popup-message");
+  document.body.appendChild(message);
+}
 
-// let quantity = parseInt(prompt("How many pizzas would u like to buy"));
-// let price = 10;
-// let result = price * quantity;
-// alert("Total amount is: " + result);
-
-// If 3 pizzas cost $30, how much would 5 pizzas cost?
-
-// let quantity = 3;
-// let cost = 30;
-// let pizzaCost = cost / quantity;
-
-// let userQty = parseInt(prompt("how many pizzas you want to order"));
-// let totalAmount = userQty * pizzaCost;
-// alert("Total cost is $" + totalAmount);
-
-//
-// let userInput = prompt("enter the secret number");
-// if (userInput == 6) {
-//   console.log("you guessed the correct number");
-// } else {
-//   console.log("you guessed the wrong number");
-// }
-
-// let message = document.getElementById("text");
-// message.innerHTML = "hello there";
-
-// let message2 = document.getElementById("text1");
-// message2.innerHTML = "welcome to my site";
-// function calculate() {
-//   let celcius = document.getElementById("celcius").value;
-
-//   let result = parseInt(celcius * (9 / 5)) + 32;
-//   let farenheit = document.getElementById("farenheit");
-//   farenheit.innerHTML = result + " F";
-// }
-
-let num1 = 5;
-let num2 = 95;
-
-console.log(`"you have ${num1} items in your cart.
-Your bill amount is $${num2}"`);
+// let a = 10;
+// console.log(a);
